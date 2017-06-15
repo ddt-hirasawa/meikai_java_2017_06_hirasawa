0be6439ab95e94550ca17c015068008c385a1dc9
hirasawa <ddt.jissyusei3@gmail.com>
2017-06-15 10:35:40 +0900

    演習1-1終了しました

---
+++ e_01_01/Sample01/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author admin
+ *
+ */
+package Sample01;
+
+public class Sample01 {
+
+
+    public static void main(String[] args){
+        System.out.println("これは、サンプルプログラムです。");
+    }
+
+}
