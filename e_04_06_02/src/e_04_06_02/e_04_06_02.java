7f556e6d0cd371acd305cdb1c9ca405f66e1e109
hirasawa <ddt.jissyusei3@gmail.com>
2017-06-15 19:12:02 +0900

    演習4-6-2終了しました

---
+++ e_04_06_02/src/e_04_06_02/e_04_06_02.java
@@ -0,0 +1,41 @@
+/* 演習4-6 List4-8を書き換え 読み込んだ値が1未満ならば改行しないように変更せよ
+ * 
+ * 作成日 2017年6月15日
+ * 
+ * 作成者 平澤敬介
+ */
+
+package e_04_06_02;
+
+import java.util.Scanner;
+
+public class e_04_06_02 {
+
+	public static void main(String[] args) {
+		
+		Scanner set_value = new Scanner(System.in);			//数値を補完する領域を確保する
+		
+		//何個 * を表示するのかユーザーに問います
+		System.out.print("*を何個表示しますか? : ");
+		int asterisk = set_value.nextInt();					//変数に * を表示する個数を補完します
+		int i = 1;												//whileループの終了条件で使う変数を定義
+		
+		//入力した値に満たない場合、繰り返される
+		while(i <= asterisk) {
+			
+			//＊を1つ表示する
+			System.out.print('*');
+			
+			i++;												//*を表示した回数をカウントする
+		}
+		
+		//入力された値が1未満ならば改行させないように if文でブロック
+		if(1 <= asterisk) {
+			//改行文字
+			System.out.println("");
+		}
+		
+		//確保している領域を解放する
+		set_value.close();
+	}
+}
