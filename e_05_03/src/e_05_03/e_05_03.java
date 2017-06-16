d8c8d9d91855431922fce9757637701c564717dc
hirasawa <ddt.jissyusei3@gmail.com>
2017-06-16 13:09:29 +0900

    演習5-3終了しました

---
+++ e_05_03/src/e_05_03/e_05_03.java
@@ -0,0 +1,22 @@
+/* 演習5-3 bool型の変数に true false を代入し 値を確認せよ
+ * 
+ * 作成日 2017年6月16日
+ * 
+ * 作成者 平澤敬介
+ */
+
+package e_05_03;
+
+public class e_05_03 {
+
+	public static void main(String[] args) {
+		
+		boolean test_bool_false = false;					//論理型の値を false で初期化
+		boolean test_bool_true= true;						//論理型の値を true で初期化
+		
+		//論理値を表示させる false
+		System.out.println("論理値 true  : " + test_bool_true);
+		//論理値を表示させる true
+		System.out.println("論理値 false : " + test_bool_false);	
+	}
+}
