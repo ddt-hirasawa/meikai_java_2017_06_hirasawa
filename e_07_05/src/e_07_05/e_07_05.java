5c162516b44191ab7de297d946856bab21f200c5
hirasawa <ddt.jissyusei3@gmail.com>
2017-06-16 20:37:18 +0900

    演習7-5終了しました

---
+++ e_07_05/src/e_07_05/e_07_05.java
@@ -0,0 +1,26 @@
+/* 演習7-5 こんにちわと表示するプログラムの作成
+ * 
+ * 作成日 2017年6月16日
+ * 
+ * 作成者 平澤敬介
+ */
+
+package e_07_05;
+
+public class e_07_05 {
+
+	public static void main(String[] args) {
+
+		//メソッド呼び出し部分
+		hello();
+	}
+	
+	//メソッド こんにちわと表示します
+	//引数 無し
+	//返却値 なし
+	static void hello() {
+		
+		//メソッド部分でこんにちは
+		System.out.println("こんにちは");
+	}
+}
